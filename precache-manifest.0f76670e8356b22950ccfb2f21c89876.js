7980a954e43ce97a1dd909bce804d5132362bd88
Moncef belhaj rhouma <mbhrhouma@neylingservices.tn>
2019-07-04 12:12:15 +0100

    Updates

---
+++ precache-manifest.0f76670e8356b22950ccfb2f21c89876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e3ba50a60bab0e68bedb78c4e544a9b",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "384c2c248f0e2d7cca12",
+    "url": "/react-tutorial/static/css/main.d37d2484.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "384c2c248f0e2d7cca12",
+    "url": "/react-tutorial/static/js/main.c3e77d70.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
